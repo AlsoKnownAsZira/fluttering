460781449078c3c12967235e6beeafa9eedf0843
Brilyan Satria Wahyuda <brilyansatria10@gmail.com>
2023-04-03 23:31:40 +0700

    Adding bottom navigation bar(Home, search setting)

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,22 @@ class MyApp extends StatelessWidget {
         title: Text("App Title!"),
        ),
        body: Text('This is BODY'),
+       bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+            label: 'Home',
+            icon : Icon(Icons.home)
+          ),
+          BottomNavigationBarItem(
+            label: 'Search',
+            icon: Icon(Icons.search)
+          ),
+          BottomNavigationBarItem(
+            label: 'Setting',
+            icon : Icon(Icons.settings)
+          )
+        ],
+       ),
       ),
     );
   }
