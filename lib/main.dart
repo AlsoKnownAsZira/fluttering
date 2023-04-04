ec14c4e711f60ee2d22d2e0d2cea42e13b3fbd34
Brilyan Satria Wahyuda <brilyansatria10@gmail.com>
2023-04-05 00:06:48 +0700

    moving body text to centre

--- lib/main.dart
+++ lib/main.dart
@@ -12,29 +12,19 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-       appBar: AppBar(
-        title:const Text("App Title!"),
-       ),
-       body: const Text('This is BODY'),
-       bottomNavigationBar: BottomNavigationBar(
-        items:const [
-          BottomNavigationBarItem(
-            label: 'Home',
-            icon : Icon(Icons.home)
-          ),
-          BottomNavigationBarItem(
-            label: 'Search',
-            icon: Icon(Icons.search)
-          ),
-          BottomNavigationBarItem(
-            label: 'Report',
-            icon : Icon(Icons.report,
-            color: Colors.red)
-          )
-        ],
-       ),
+        appBar: AppBar(
+          title: const Text("App Title!"),
+        ),
+        body: const Center(child:  Text('This is BODY')),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const [
+            BottomNavigationBarItem(label: 'Home', icon: Icon(Icons.home)),
+            BottomNavigationBarItem(label: 'Search', icon: Icon(Icons.search)),
+            BottomNavigationBarItem(
+                label: 'Report', icon: Icon(Icons.report, color: Colors.red))
+          ],
+        ),
       ),
     );
   }
 }
-
